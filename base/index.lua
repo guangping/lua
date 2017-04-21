8ddff6cfa4e913b63e6488d64734e4ada8db5dfa
mgp <mgp@edidding.com.cn>
2017-04-21 18:09:05 +0800

    调整

---
+++ base/index.lua
@@ -0,0 +1,12 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: lance
+-- Date: 2017/04/21 17:31
+-- Desc:基础练习部分
+
+
+ngx.say('ffff');
+ngx.say(ngx.var.remote_addr);
+ngx.say(ngx.var.server_protocol);
+ngx.say(ngx.var.uri);
+
